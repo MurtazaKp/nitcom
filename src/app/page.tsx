8f986495a799150ea5e4815d3078769489079898
MurtazaKp <Khopoliwala52@gmail.com>
2025-02-01 14:49:06 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import ClientsSection from "@/components/client";
 import Contact from "@/components/contact";
 import Cta from "@/components/cta";
 import FAQ from "@/components/faq";
-import Features from "@/components/Features";
+import Features from "@/components/features";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import HeroSection from "@/components/herosection";
